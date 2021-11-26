8b5b4b2bd7a273277da11285982e7a68f3ddad26
Craig Cahillane <ccahilla@gmail.com>
2021-11-25 23:36:03 -0600

    Added limit order size

--- check_limit_orders.py
+++ check_limit_orders.py
@@ -5,11 +5,13 @@ Finds and prints your current limit orders on the current portfolio API in .env
 Craig Cahillane
 Nov 25, 2021
 """
+PRODUCT = "ETH-USD"
+BUYSELL = PRODUCT.split("-")
+BUY = BUYSELL[0]
+SELL = BUYSELL[1]
 
 import cbpro
 from dotenv import dotenv_values
-
-PRODUCT = "ETH-USD"
 
 # initialize API
 config = dotenv_values(".env")
@@ -21,6 +23,7 @@ auth_client = cbpro.AuthenticatedClient(key, b64secret, passphrase)
 # Get user limit orders
 orders = auth_client.get_orders()
 list_orders = list(orders)
+print(list_orders)
 
 # retrieve current price
 order_book = auth_client.get_product_order_book(PRODUCT)
@@ -31,28 +34,34 @@ current_price = round((current_bid + current_ask) / 2, 2)
 print("\033[92m")
 print("Limit Sells")
 print("===========")
+print("Sell price   Sell size")
 print("\033[0m", end="")
 for order in list_orders:
     if order["side"] == "sell":
+        temp_size = float(order["size"]) 
         temp_price = round(float(order["price"]), 2)
+        temp_size_price = round(temp_size * temp_price, 2)
         temp_diff = round(temp_price - current_price, 2)
         temp_percent_diff = 100 * temp_diff / current_price
-        print(f"{temp_price}   (\033[92m +{temp_diff} \033[0m) (\033[92m {temp_percent_diff:.1f} % \033[0m)")
+        print(f"{temp_price} USD  {temp_size_price} USD  (\033[92m +{temp_diff} \033[0m) (\033[92m {temp_percent_diff:.1f} % \033[0m)")
 
 print("\033[96m")
 print("Current Price")
 print("=============")
 print("\033[0m", end="")
-print(current_price)
+print(f"{current_price} USD")
 
 print("\033[91m")
 print("Limit Buys")
-print("==========", end="")
-print("\033[0m")
+print("==========")
+print("Buy price    Buy size")
+print("\033[0m", end="")
 for order in list_orders:
     if order["side"] == "buy":
+        temp_size = float(order["size"]) 
         temp_price = round(float(order["price"]), 2)
+        temp_size_price = round(temp_size * temp_price, 2)
         temp_diff = round(temp_price - current_price, 2)
         temp_percent_diff = 100 * temp_diff / current_price
-        print(f"{temp_price}   (\033[91m {temp_diff} \033[0m) (\033[91m {temp_percent_diff:.1f} % \033[0m)")
+        print(f"{temp_price} USD  {temp_size_price} USD  (\033[91m {temp_diff} \033[0m) (\033[91m {temp_percent_diff:.1f} % \033[0m)")
 print()
