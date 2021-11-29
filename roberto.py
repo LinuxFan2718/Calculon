246d953da1b3a0872e420393da998e9a053ebd93
Dennis Cahillane <dennis@thirtymadison.com>
2021-11-28 19:28:10 -0600

    prompt user for confirmation

--- roberto.py
+++ roberto.py
@@ -1,8 +1,6 @@
 BUY_AMOUNT_USD = 100.0
 PRODUCT = "ETH-USD"
 SWING_SIZE = 0.05
-DEBUG = True
-PLACE_ORDERS = True
 
 import cbpro
 from dotenv import dotenv_values
@@ -28,18 +26,12 @@ sell_price = round((1 + SWING_SIZE) * current_price, 2)
 buy_amount = round(BUY_AMOUNT_USD / current_price, 8)
 sell_amount = round(BUY_AMOUNT_USD / current_price, 8)
 
-if DEBUG:
-  print(f"buy price      {buy_price} $")
-  print(f"current price  {current_price} $")
-  print(f"sell price     {sell_price} $")
-
-  product_traded = PRODUCT.split('-')[0]
-  print(f"buy amount     {buy_amount} {product_traded}")
-  print(f"buy amount     {sell_amount} {product_traded}")
-
-# create limit orders
-# Limit order-specific method
-if PLACE_ORDERS:
+print(f"{PRODUCT} current price: ${current_price}")
+print(f"buy  {PRODUCT} ${buy_price} ({buy_amount})")
+print(f"sell {PRODUCT} ${sell_price} ({sell_amount})")
+print("place limit orders? (y/n)")
+ans = input()
+if ans == 'y':
   buy_order = auth_client.place_limit_order(product_id=PRODUCT,
                                 side='buy',
                                 price=buy_price,
@@ -54,39 +46,41 @@ if PLACE_ORDERS:
     print('buy error:', buy_order['message'])
   if 'message' in sell_order:
     print('sell error:', sell_order['message'])
-
-orders = auth_client.get_orders()
-list_orders = list(orders)
+else:
+  print(f"orders aborted")
 
 # Pretty print limit orders and current price
-print("\033[92m")
-print("Limit Sells")
-print("===========")
-print("\033[0m", end="")
-for order in list_orders:
-    if order["side"] == "sell":
-        temp_price = round(float(order["price"]), 2)
-        temp_diff = round(temp_price - current_price, 2)
-        temp_percent_diff = 100 * temp_diff / current_price
-        print(f"{temp_price}   (\033[92m +{temp_diff} \033[0m) (\033[92m {temp_percent_diff:.3f} % \033[0m)")
+def print_order_book():
+  orders = auth_client.get_orders()
+  list_orders = list(orders)
+  print("\033[92m")
+  print("Limit Sells")
+  print("===========")
+  print("\033[0m", end="")
+  for order in list_orders:
+      if order["side"] == "sell":
+          temp_price = round(float(order["price"]), 2)
+          temp_diff = round(temp_price - current_price, 2)
+          temp_percent_diff = 100 * temp_diff / current_price
+          print(f"{temp_price}   (\033[92m +{temp_diff} \033[0m) (\033[92m {temp_percent_diff:.3f} % \033[0m)")
 
-print("\033[96m")
-print("Current Price")
-print("=============")
-print("\033[0m", end="")
-print(current_price)
+  print("\033[96m")
+  print("Current Price")
+  print("=============")
+  print("\033[0m", end="")
+  print(current_price)
 
-print("\033[91m")
-print("Limit Buys")
-print("==========", end="")
-print("\033[0m")
-for order in list_orders:
-    if order["side"] == "buy":
-        temp_price = round(float(order["price"]), 2)
-        temp_diff = round(temp_price - current_price, 2)
-        temp_percent_diff = 100 * temp_diff / current_price
-        print(f"{temp_price}   (\033[91m {temp_diff} \033[0m) (\033[91m {temp_percent_diff:.3f} % \033[0m)")
-print()
+  print("\033[91m")
+  print("Limit Buys")
+  print("==========", end="")
+  print("\033[0m")
+  for order in list_orders:
+      if order["side"] == "buy":
+          temp_price = round(float(order["price"]), 2)
+          temp_diff = round(temp_price - current_price, 2)
+          temp_percent_diff = 100 * temp_diff / current_price
+          print(f"{temp_price}   (\033[91m {temp_diff} \033[0m) (\033[91m {temp_percent_diff:.3f} % \033[0m)")
+  print()
 
 # sample order
 # 'id':'3db19014-f97d-449c-b40e-2122a1d73e50'
