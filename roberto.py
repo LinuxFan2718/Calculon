c0cf962b179f29321545e4a97d91a17ea32970fd
Craig Cahillane <craig.cahillane@ligo.org>
2021-12-15 01:25:59 -0800

    Added a fiat_profits_percent command arg to roberto.py, but this is still wrongheaded... you can't define a ratio and the buy_amount_usd for all ratios, only for ratio = 0.0   We are of course changing our buy amounts by swing_size, so already we're doing something weird.

--- roberto.py
+++ roberto.py
@@ -13,6 +13,8 @@ def parse_args():
                         help='Amount of crypto to buy in US dollars')
     parser.add_argument('swing_percent', type=float, default=5.0,
                         help='Percent to set limit orders above and below.  Must be between 0 and 100.  Default is 5.0')
+    parser.add_argument('fiat_profits_percent', type=float, default=100.0,
+                        help='Percent of profits to keep in fiat.  Must be between 0 and 100.  Default is 100.0')
     parser.add_argument('--yes', '-y', action='store_true',
                         help='Flag.  If set, does not need confirmation to set up limit orders.')
     parser.add_argument('--quiet', '-q', action='store_true',
@@ -24,20 +26,24 @@ def parse_args():
     product = args.product
     buy_amount_usd = args.buy_amount_usd
     swing_percent = args.swing_percent
+    fiat_profits_percent = args.fiat_profits_percent
     yes = args.yes
     quiet = args.quiet
 
+    # basic sanitation
+
     if not quiet:
-        print(f"product         = {product}")
-        print(f"buy_amount_usd  = {buy_amount_usd}")
-        print(f"swing_percent   = {swing_percent}")
-        print(f"yes             = {yes}")
-        print(f"quiet           = {quiet}")
+        print(f"product              = {product}")
+        print(f"buy_amount_usd       = {buy_amount_usd}")
+        print(f"swing_percent        = {swing_percent}")
+        print(f"fiat_profits_percent = {fiat_profits_percent}")
+        print(f"yes                  = {yes}")
+        print(f"quiet                = {quiet}")
 
-    return product, buy_amount_usd, swing_percent, yes, quiet
+    return product, buy_amount_usd, swing_percent, fiat_profits_percent, yes, quiet
 
 
-def set_limit_orders(product, buy_amount_usd, swing_percent, yes, quiet):
+def set_limit_orders(product, buy_amount_usd, swing_percent, fiat_profits_percent, yes, quiet):
     """main function of roberto.py.
     Sets the buy low / sell high limit orders for the cryptocurrency product specified.
 
@@ -49,6 +55,8 @@ def set_limit_orders(product, buy_amount_usd, swing_percent, yes, quiet):
         Central amount of cryptocurrency to trade at the current price, e.g. 1000.0
     swing_percent: float
         Percent amount above and below the current price to set the limit orders at, e.g. 5.0
+    fiat_profits_percent: float
+        Percent of profits to keep in fiat, e.g. 100.0
     yes: bool
         Flag. If set, user will not need to manually type 'y' to activate limit orders
     quiet: bool
@@ -63,12 +71,21 @@ def set_limit_orders(product, buy_amount_usd, swing_percent, yes, quiet):
     """
 
     # Check out user inputs
-    if swing_percent < 0.0:
+    if swing_percent < 0.0 or swing_percent > 100.0:
+        print()
         print(f"swing_percent = {swing_percent} is not valid")
-    elif swing_percent > 100.0:
-        print(f"swing_percent = {swing_percent} is not valid")
+        print("no orders placed")
+        return
     else:
         swing_size = swing_percent / 100.0
+
+    if fiat_profits_percent < 0.0 or fiat_profits_percent > 100.0:
+        print()
+        print(f"fiat_profits_percent = {fiat_profits_percent} is not valid")
+        print("no orders placed")
+        return
+    else:
+        ratio_profits_in_fiat = fiat_profits_percent / 100.0
 
     # initialize
     config = dotenv_values(".env")
@@ -93,9 +110,12 @@ def set_limit_orders(product, buy_amount_usd, swing_percent, yes, quiet):
         elif product == 'ETH-USD':
             return 8
 
+    # decide how much profits to keep in crypto versus fiat
+    # ratio_profits_in_fiat = 1.0
+
     # calculate amount, i.e. "size", of crypto to buy, for now the same
-    buy_amount = round(buy_amount_usd / current_price, round_size(product))
-    sell_amount = round(buy_amount_usd / current_price, round_size(product))
+    buy_size = round(buy_amount_usd * (1 - ratio_profits_in_fiat * swing_size) / buy_price, round_size(product))
+    sell_size = round(buy_amount_usd * (1 + ratio_profits_in_fiat * swing_size) / sell_price, round_size(product))
 
     crypto = product.split('-')[0]
     if not quiet:
@@ -103,10 +123,10 @@ def set_limit_orders(product, buy_amount_usd, swing_percent, yes, quiet):
         print(f"{crypto} current price: ${current_price}")
         print("\033[0m", end="")
         print("\033[92m", end="")
-        print(f"buy  {product} ${buy_price} ${buy_price*buy_amount} ({buy_amount} {crypto})")
+        print(f"buy  {product} ${buy_price} ${buy_price * buy_size} ({buy_size} {crypto})")
         print("\033[0m", end="")
         print("\033[91m", end="")
-        print(f"sell {product} ${sell_price} ${sell_price*sell_amount} ({sell_amount} {crypto})")
+        print(f"sell {product} ${sell_price} ${sell_price * sell_size} ({sell_size} {crypto})")
         print("\033[0m", end="")
 
     if yes:
@@ -119,12 +139,12 @@ def set_limit_orders(product, buy_amount_usd, swing_percent, yes, quiet):
         buy_order = auth_client.place_limit_order(product_id=product,
                                         side='buy',
                                         price=buy_price,
-                                        size=buy_amount)
+                                        size=buy_size)
 
         sell_order = auth_client.place_limit_order(product_id=product,
                                         side='sell',
                                         price=sell_price,
-                                        size=sell_amount)
+                                        size=sell_size)
 
         if 'message' in buy_order:
             print('buy error:', buy_order['message'])
@@ -132,12 +152,13 @@ def set_limit_orders(product, buy_amount_usd, swing_percent, yes, quiet):
             print('sell error:', sell_order['message'])
     else:
         print(f"orders aborted")
+        return 
 
     return buy_order, sell_order
 
 if __name__ == "__main__":
-    product, buy_amount_usd, swing_percent, yes, quiet = parse_args()
-    set_limit_orders(product, buy_amount_usd, swing_percent, yes, quiet)
+    product, buy_amount_usd, swing_percent, fiat_profits_percent, yes, quiet = parse_args()
+    set_limit_orders(product, buy_amount_usd, swing_percent, fiat_profits_percent, yes, quiet)
 
 # sample order
 # 'id':'3db19014-f97d-449c-b40e-2122a1d73e50'
